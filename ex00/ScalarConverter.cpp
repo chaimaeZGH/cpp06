251f73cf1910da7b7a3443f23f647710945eae00
Chaimae Zghoumi <czghoumi@c1r6p7.1337.ma>
2026-03-27 12:32:01 +0100

    maybe

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -1,7 +1,6 @@
 #include "ScalarConverter.hpp"
 #include <string>
-#include <algorithm>
-#include <set>
+
 
 ScalarConverter::ScalarConverter(const ScalarConverter &other)
 {
@@ -48,7 +47,7 @@ int check_n(std::string& arg)
 int check_char(std::string& arg)
 {
     int a = std::atoi(arg.c_str());
-        if ( a == 0) 
+     if ( a == 0) 
         {
             std::cout<<"char: "<< static_cast<char>(arg[0]) <<std::endl;
             std::cout<< "int: "<< static_cast<int>(arg[0])<<std::endl;
@@ -56,7 +55,7 @@ int check_char(std::string& arg)
             std::cout<<"double: "<< static_cast<double>(arg[0])<<".0"<<std::endl;
             return 1;
         }
-        else if(a < 32 || a>128)
+        if(a < 32 || a > 126)
         {
             std::cout<<"char: "<< "Non displayable" <<std::endl;
             std::cout<< "int: "<< static_cast<int>(a)<<std::endl;
@@ -72,40 +71,60 @@ int check_char(std::string& arg)
             std::cout<<"double: "<< static_cast<double>(a)<<".0"<<std::endl;
             return 1;
         }
-        
     return 0;
 }
 
 int check_float(std::string& arg)
 {
-    
+    float f = std::strtof(arg.c_str(), NULL);
     int a = std::atoi(arg.c_str());
-    if(a>128 || a<32)
+    if(a > 126 || a < 32)
         std::cout<<"char: "<< "Non displayable" <<std::endl;
     else
         std::cout<<"char: "<< static_cast<char>(a) <<std::endl;
     std::cout<< "int: "<< static_cast<int>(a)<<std::endl;
-    std::cout<<"float: "<< arg <<std::endl;
-    std::cout<<"double: "<< arg <<std::endl;//this wrong
+    std::cout<<"float: "<< f << "f"<<std::endl;
+    std::cout<<"double: "<< static_cast<double>(f) <<std::endl;
     return 1;
 }
 
 int check_double(std::string& arg)
 {
+    double d = std::strtod(arg.c_str(), NULL);
     int a = std::atoi(arg.c_str());
-    if(a > 128 || a < 32)
+    if(a > 126 || a < 32)
         std::cout<<"char: "<< "Non displayable" <<std::endl;
     else
         std::cout<<"char: "<< static_cast<char>(a) <<std::endl;
     std::cout<< "int: "<< static_cast<int>(a)<<std::endl;
-    std::cout<<"float: "<< arg <<"f"<<std::endl;//this wrong
-    std::cout<<"double: "<< arg <<std::endl;
+    std::cout<<"float: "<< static_cast<float>(d)<<"f"<<std::endl;
+    std::cout<<"double: "<< d <<std::endl;
     return 1;
+}
+
+bool valid_num(const std::string& str)
+{
+    char delimiter = '.';
+    if(std::count(str.begin(), str.end(), '.') > 1)
+        return false;
+    size_t dotPos = str.find(delimiter);
+    std::string first = str.substr(0, dotPos);
+    std::string second = str.substr(dotPos + 1);
+    if(!std::all_of(first.begin(), first.end(), ::isdigit) || !std::all_of(second .begin(), second .end(), ::isdigit) )
+        return false;
+    return true;
 }
 
 void ScalarConverter::convert(const std::string& str)
 {
     std::string arg = str;
+    int a = std::atoi(arg.c_str());
+    bool valid = valid_num(str);
+    if((arg.length() > 1 && !valid) || (std::count(arg.begin(), arg.end(), 'e') > 0 && a > 0))
+    {
+        std::cout << "argument is incorect"<<std::endl;
+        return;
+    }
     if(arg.empty())
     {
         std::cout << "argument is empty"<<std::endl;
@@ -115,11 +134,6 @@ void ScalarConverter::convert(const std::string& str)
         return ;
     if (std::count(arg.begin(), arg.end(), '.') > 0 ) 
     {
-        if(std::count(arg.begin(), arg.end(), '.') > 1)
-        {
-            std::cout << "argument is incorect"<<std::endl;
-            return;
-        }
         if(std::count(arg.begin(), arg.end(), 'f')==1)
         {
             if(check_float(arg))
@@ -132,3 +146,5 @@ void ScalarConverter::convert(const std::string& str)
         return ;
     return ;
 }
+// overflo
+// 0.0
